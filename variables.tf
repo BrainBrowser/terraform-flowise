715d18b64f9ef4e6151c1274268a9a19e3099cd6
Himanshu Mittal <himanshu.mittal.dev@gmail.com>
2026-03-09 10:02:47 +0530

    update: simplify aws architecture

--- variables.tf
+++ variables.tf
@@ -1,11 +1,17 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "ap-northeast-2"  # Seoul region
+  default     = "us-east-1"  # Seoul region
 }
 
 variable "stage" {
   description = "Prefix for resource names"
   type        = string
   default     = "flowise"
+}
+
+variable "allowed_cidr" {
+  description = "CIDR allowed to access Flowise on port 3000. Use your IP: curl ifconfig.me"
+  type        = string
+  default     = "0.0.0.0/0" # Restrict to your IP for security, e.g. "203.0.113.5/32"
 }
